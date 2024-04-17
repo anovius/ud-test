103653514367b3ba9f5be99b38ec4d7f908589b2
Anovius <anovius10@gmail.com>
2024-04-17 19:15:24 +0500

    service setup

--- src/services/python.service.ts
+++ src/services/python.service.ts
@@ -1,6 +1,29 @@
 import axios from "axios";
-let URL = ''
-const callPythonAPI = async (data: any) => {
-    const response = await axios.post(URL, data);
+let URL = 'https://api.undetectable.ai/'
+
+//set up api-key and content-type in axios header
+
+axios.defaults.headers.common['api-key'] = 'API_KEY';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+const submitDocument = async (body: any, params: any) => {
+    const response = await axios.post(URL + 'submit-document', body, { params });
     return response.data;
-};
+}
+
+const retrieveDocument = async (id: any) => {
+    const response = await axios.get(URL + 'document', { params: { id } });
+    return response.data;
+}
+
+const listDocuments = async (page: Number) => {
+    const response = await axios.get(URL + 'list', { params: { page } });
+    return response.data;
+}
+
+export default {
+    submitDocument,
+    retrieveDocument,
+    listDocuments
+}
