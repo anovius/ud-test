2ac1bae9350319c203b04c66cbd257ca4baec320
Anovius <anovius10@gmail.com>
2024-04-18 16:04:25 +0500

    env file setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+console.log("firebase.ts", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCthDKs2WCcaGq8Qj4d8EzxO29Uv4uLmRA",
-    authDomain: "ud-test-f2ae9.firebaseapp.com",
-    projectId: "ud-test-f2ae9",
-    storageBucket: "ud-test-f2ae9.appspot.com",
-    messagingSenderId: "1038698894838",
-    appId: "1:1038698894838:web:81ccb3ad3cbdb0c4a4e3d7",
-    measurementId: "G-YK4BJTC9BG"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
